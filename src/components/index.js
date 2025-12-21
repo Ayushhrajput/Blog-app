d42a43e35da4afbe6c55f960f46969bf16573a3f
Ayushhrajput <ayushrajput939@gmail.com>
2025-12-21 05:55:12 +0530

    optimised stuff

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,7 @@ import Header from "./header/Header";
 import Footer from "./footer/Footer";
 import Logo from "./Logo";
 import LogoutBtn from "./header/LogoutBtn";
-import Container from "./container/container";
+import Container from "./container/Container";
 import Button from "./Button";
 import Input from "./Input";
 import Select from "./Select";
